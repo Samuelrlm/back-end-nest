176008159b0297038ca559d133391cc21c64af34
Samuel Lima <202302913458@alunos.unifanor.edu.br>
2023-10-06 01:45:32 -0300

    Ajuste session user

--- src/getway/getway.module.ts
+++ src/getway/getway.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { MyGateway } from './gateway';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/schemas/user.schema';
+import { SessionUserSchema } from 'src/schemas/session.user.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: 'SessionUser', schema: SessionUserSchema },
+    ]),
+  ],
   providers: [MyGateway],
 })
 export class GetwayModule {}
